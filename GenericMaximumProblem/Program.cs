56bb5a8f4b26350f73e64168f5dd7a4642a0a424
SohailLuffy <sohailislammail97@gmail.com>
2022-11-29 18:58:11 +0530

    Welcome

---
+++ GenericMaximumProblem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericMaximumProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Maximum Problems");
+        }
+    }
+}
