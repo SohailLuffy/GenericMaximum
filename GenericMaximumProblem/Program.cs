0adafe50b1433bb833e943da9347f391361e46c6
SohailLuffy <sohailislammail97@gmail.com>
2022-11-29 19:18:23 +0530

    Find Maximum of 3 Strings

--- GenericMaximumProblem/Program.cs
+++ GenericMaximumProblem/Program.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 
-namespace FindMaxOf3Float
+namespace FindMaxOf3String
 {
-    internal class MaxOf3Float
+    internal class MaxOf3String
     {
-        public static float findmaxfloat(float first, float second, float third)
+        public static String findmaxstring(String first, String second, String third)
         {
-            //If first-second>0 && first-third>0
             if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0 ||
                first.CompareTo(second) >= 0 && first.CompareTo(third) > 0 ||
                first.CompareTo(second) > 0 && first.CompareTo(third) >= 0)
             {
                 return first;
             }
-            //If second-first>0 && second-third>0
             if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) >= 0 && second.CompareTo(third) > 0 ||
                 second.CompareTo(first) > 0 && second.CompareTo(third) >= 0)
             {
                 return second;
             }
-            //If third-first>0 && third-second>0
             if (third.CompareTo(second) > 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) >= 0 && first.CompareTo(first) > 0 ||
                 third.CompareTo(second) > 0 && first.CompareTo(first) >= 0)
@@ -31,7 +28,7 @@ namespace FindMaxOf3Float
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum Float Number Is:" + findmaxfloat(12.5f, 8.34f, 15.74f));
+            Console.WriteLine("Maximum String Is:" + findmaxstring("RRR", "AAA", "BBB"));
         }
     }
 }
